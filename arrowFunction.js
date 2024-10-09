a86c20add87364a9bc541d287a91eb960394f535
Saja-Albinali <sajaalbinali176@gmail.com>
2024-10-09 07:56:29 +0300

    commit arrowFunctions

---
+++ arrowFunction.js
@@ -0,0 +1,9 @@
+// task1
+const greet = (name) => `Hello, ${name}!`;
+// task2
+const sum = (x, y) => x + y;
+console.log(sum(3, 5));
+// task3
+const square = (x) => x * x;
+console.log(square(4)); 
+console.log(square(5)); 
